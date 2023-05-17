5fafacfc6664079dd425eabd157a75233b837f5a
Rafaat Anayat <rafaatanayat@gmail.com>
2023-05-16 21:31:02 -0400

    Practice on y own

--- src/com/hello/AnotherJavaClass.java
+++ src/com/hello/AnotherJavaClass.java
@@ -2,7 +2,7 @@ package com.hello;
 
 public class AnotherJavaClass {
 	public static void main(String[] args) {
-		System.out.println("Another Example");
+		System.out.println("Another class added");
 	}
 
 }
---
+++ src/com/hello/PracticeClass.java
@@ -0,0 +1,8 @@
+package com.hello;
+
+public class PracticeClass {
+	public static void main(String[] args) {
+		System.out.println("Practice on my own");
+	}
+
+}
